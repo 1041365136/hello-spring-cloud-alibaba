28ed2b168978bbc8cf2fe4076a1c3509855b8b08
erythron <1041365136@qq.com>
2020-06-26 21:25:18 +0800

    添加了lombok logback配置 提供者的日志打印

--- spring-cloud-alibaba-nacos/spring-cloud-alibaba-nacos-provider/src/main/java/com/ery/nacos/provider/controller/ProviderController.java
+++ spring-cloud-alibaba-nacos/spring-cloud-alibaba-nacos-provider/src/main/java/com/ery/nacos/provider/controller/ProviderController.java
@@ -1,13 +1,16 @@
 package com.ery.nacos.provider.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class ProviderController {
 
     @RequestMapping("sayHi")
     public String sayHi(){
+        log.debug("provider 返回了字符串");
         return "Hello Spring Cloud Nacos Provider";
     }
 
